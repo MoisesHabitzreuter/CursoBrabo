238dd1bf927f7c6c71e2a4cf526757289c3ce57e
louyza <louyzadut@gmail.com>
2020-01-31 14:52:00 -0300

    Segundo commit

--- GitTeste/GitTeste/Program.cs
+++ GitTeste/GitTeste/Program.cs
@@ -10,7 +10,7 @@ namespace GitTeste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("o curso é muito brabo!!!!");
+            Console.WriteLine("o curso é muito brabo!!!!!!!!");
 
         }
     }
